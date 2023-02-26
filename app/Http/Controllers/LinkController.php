71f0d313445a5ffe71b6bba0620e05df694b435f
Tyler Darnell <tyler.a.darnell@gmail.com>
2023-02-26 16:53:03 -0600

    Add IP metadata tracking to shortlink visits.

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -7,7 +7,6 @@ use Illuminate\View\View;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Log;
 
 use App\Models\User;
 use App\Models\Link;
